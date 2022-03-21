c2b1174e306acd1ceabbcc42643298b095c1acbb
squiggs <brennan@bell.net>
2022-03-21 19:19:08 -0400

    add css

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Link from 'next/link'
-import Footer from '../comp/footer';
-import Navbar from '../comp/navbar';
+
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return ( 
 
     <div>
-    <Navbar />
-        <Link a href="/cont/northamerica"><a> North America</a></Link> <br />
+    
+        <Link a href="/cont/northamerica"><a> North America</a></Link> <br /> 
         <Link a href="/cont/southamerica"><a> South America</a></Link>
-    <Footer />
+  
         </div>
    );
 }
